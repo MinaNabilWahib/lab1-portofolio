9718c5832cb8a8ef7f97ddd331132c37931a5db1
Mina Nabil <minanabilsalib@gmail.com>
2022-03-25 17:22:42 +0200

    nav bar

--- src/Navbar.js
+++ src/Navbar.js
@@ -2,7 +2,7 @@ function Navbar() {
     return (
         <div className="navbar-container">
             <nav className="navbar navbar-expand-lg navbar-light bg-light">
-                <a className="navbar-brand" href="home">Navbar</a>
+                <a className="navbar-brand" href="home">Hola</a>
                 <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
@@ -18,7 +18,7 @@ function Navbar() {
                             <a className="nav-link" href="#skills">Skills</a>
                         </li>
                         <li className="nav-item">
-                            <span className="nav-link">Portofolio</span>
+                            <a className="nav-link" href="#portofolio">Portofolio</a>
                         </li>
                     </ul>
                 </div>
